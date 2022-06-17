14d1e38181ad38fa54d0bb53575254db0d4a3297
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-06-17 16:27:22 +0530

    check for invalid in c?

---
+++ ques1.c
@@ -0,0 +1,15 @@
+//Which of the following is invalid in c?
+# include<stdio.h>
+# include <math.h>
+
+int main(){
+    //int a; b=a;//wrong
+    //printf("%d",a);
+
+ //int v=pow(3^3);//right
+ //printf("%d",v);
+
+ char dt= '21 Dec 2020' ;
+ printf("%d",dt);
+ 
+}
