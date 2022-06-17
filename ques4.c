1ee9e6a8e0bbf4608f929cc1c9d7f0d4bd8ae09a
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-06-17 16:35:42 +0530

    step by step evaluation of 3*x/y-z +k

---
+++ ques4.c
@@ -0,0 +1,11 @@
+//Explain step by step evaluation of 3*x/y-z +k
+
+//Where x=2, y=3, z=3 and k=1
+# include<stdio.h>
+# include <math.h>
+
+int main(){
+  int  x=2, y=3, z=3, k=1;
+  int result=3*x/y-z +k;
+  printf("%d",result);
+}
