aefaa56ca18cb153113872f91596c056e39c95d1
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-06-17 16:34:04 +0530

    check whether a number is divisible  97 or not?

---
+++ ques3.c
@@ -0,0 +1,11 @@
+//Write a program to check whether a number is divisible  97 or not.
+# include<stdio.h>
+# include <math.h>
+
+int main(){
+    int num;
+    printf("Enter the number\n");
+    scanf("%d",&num);
+    printf("%d\n",num%97);
+    return 0;
+}
