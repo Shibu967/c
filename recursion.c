1dafbf12074aef3504015268492dc188b4f42d4d
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-07-02 19:38:34 +0530

    recursion

---
+++ recursion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int factorial(int x);
+
+int main()
+{
+    int a = 3;
+    printf("the value of factorial %d is %d", a, factorial(a));
+
+    return 0;
+}
+int factorial(int x)
+{
+    if (x == 1 || x == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return x * factorial(x - 1);
+    }
+}
