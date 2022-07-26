43c62581d519382c93d7cc5642a44f2c6c2098ca
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-07-26 19:55:08 +0530

    area of circle

---
+++ circle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int radius = 2;
+    float pi = 3.14;
+    int area = pi * radius * radius;
+    printf("print the area of circle %d\n", area);
+    int height = 3;
+    printf("Volume of this cylinder is %f\n", pi * radius * radius * height);
+
+    return 0;
+}
